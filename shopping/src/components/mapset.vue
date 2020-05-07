fd7715bb434446f0eb59381cb3e64c7143c4e343
Xiamengmeng <940503239@qq.com>
2020-05-07 22:34:41 +0800

    css

--- shopping/src/components/mapset.vue
+++ shopping/src/components/mapset.vue
@@ -1,18 +1,40 @@
 
 <template>
-    <section>
+    <section class="section">
         <h1>wwwww</h1>
+        <a href="tel:18501750726" id="phone1" @click="s">
+            <div class="tel">
+                电话
+            </div>
+        </a>
     </section>
 </template>
 <script>
 export default{
-    data(){
-        return {
-            
-        }
-    }
+   data(){
+       return{}
+   },
+   methods:{
+       s(){
+           console.log("aa")
+       }
+   }
+   
 }
 </script>
 <style>
-
+.section{
+    /* position: relative; */
+}
+.tel{
+    width: 100px;
+    height: 100px;
+    position: fixed;
+    float: right;
+    /* right: 20px;
+    bottom: 20px; */
+    background-color: mediumturquoise;
+    top: 20px;
+    left: 20px;
+}
 </style>
